054a0c0cea4e3c769261867510ff32e1db0716ea
Pasi J <pasi.husso@gmail.com>
2019-05-03 20:13:45 +0300

    Add working bisect setup!

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,12 +7,21 @@ node {
         echo "PARAMETERS"
         echo "=========="
         echo "$params.first_commit"
-        git branch: 'master',
-            url: 'https://github.com/hussopa/SWD4TA002-3001/'
+        git branch: 'master'
+        git bisect: 'reset'
     }
     
     stage('Bisect'){
-        gitbisect
+        gitbisect badEndCommit: "$params.last_commit", 
+            continuesBuild: true, 
+            gitCommand: 'git', 
+            goodStartCommit: "$params.first_commit", 
+            jobToRun: 'tester', 
+            minSuccessfulIterations: 1, 
+            overrideGitCommand: false, 
+            retryCount: 0, 
+            revisionParameterName: 'REVISION', 
+            searchIdentifier: "$params.search"
     }
     
     stage('Build'){
